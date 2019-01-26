a7e63926748ef9a7e7ba6400d5be9787778c4e20
osogrizz <lhtor@gmail.com>
2019-01-25 21:23:28 -0800

    Updated Header to stateful component.

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,50 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { Component } from 'react'
+import { Transition } from 'react-spring'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+
+class Header extends Component {
+  state = {
+    items: [],
+
+  }
+
+
+  render() {
+    const { siteTitle } = this.props
+    return (
+      <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `inherit`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 1420,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >  
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `red`,
+              textDecoration: `none`,
+            }}
+            >
+              {siteTitle}
+          </Link>
+        </h1>
+      </div>
+    </div> 
+    )
+  }
+}
+
+export default Header
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -39,4 +54,7 @@ Header.defaultProps = {
   siteTitle: ``,
 }
 
-export default Header
+
+
+
+
