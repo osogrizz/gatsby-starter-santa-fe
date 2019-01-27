7d9faf4139d831138a0d3157f0d607884c6ff5c1
osogrizz <lhtor@gmail.com>
2019-01-26 16:33:50 -0800

    Fixed contact page SEO title.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ import SEO from '../components/seo'
 
 const Contact = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="Contact" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
