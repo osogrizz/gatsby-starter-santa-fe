b561cfe77f87da2e84ab64eb3b3ced7ab94c3026
osogrizz <lhtor@gmail.com>
2019-01-26 16:21:29 -0800

    Added main bag-theme image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,17 @@ import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
+import BagImage from '../components/bag-image'
 import SEO from '../components/seo'
+import { delay } from 'q';
 
 const Wrapper = styled.div`
   display: grid;
   text-align: center;
 
+`
+const BagImgStyles = styled.div`
+  border: 8px solid rgb(243, 235, 222);
 `
 
 
@@ -21,7 +26,7 @@ class IndexPage extends Component {
         <Layout>
           <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
 
-          <Spring config={config.stiff}
+          <Spring config={ config.stiff }
             from={{marginLeft: -2000, fontSize: 200}}
             to={{marginLeft: 0, fontSize: 200}}
           >
@@ -32,10 +37,15 @@ class IndexPage extends Component {
           )}
           </Spring>
 
-          <div style={{ paddingTop: `100px`, margin: `0 auto`, maxWidth: `243px`, marginBottom: `1.45rem` }}>
+          <div style={{ paddingTop: `100px`, margin: `0 auto`, maxWidth: `230px`, marginBottom: `1.45rem` }}>
             <Image />
           </div>
+
           <p>A BAG SHOULD BE A SCULPTURE BEFORE ANYTHING ELSE</p>
+
+          <BagImgStyles style={{  margin: `0 auto`, maxWidth: `660px`,maxHeight: `500px`, marginBottom: `1.45rem` }}>
+            <BagImage />
+          </BagImgStyles>
       <Link to="/contact/">Contact</Link>
     </Layout>
     </Wrapper>
