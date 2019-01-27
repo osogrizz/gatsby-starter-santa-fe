0ac78ea87b0cef28bab653d74571cf3071453417
osogrizz <lhtor@gmail.com>
2019-01-26 20:56:27 -0800

    Added initial form to contact page.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,36 @@
 import React from 'react'
 // import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+const FormContainer = styled.div`
+  margin: 0 auto;
+  display: grid;
+  max-width: 700px;
+  grid-template-columns: 1fr;
+  text-align: center;
+`
+
 const Contact = () => (
   <Layout>
-    <SEO title="Contact" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+      <SEO title="Contact" />
+      
+      <form action="">
+        <FormContainer>
 
-  </Layout>
+        <input type="name" placeholder="Name"/>
+
+
+        <input type="email" placeholder="Email"/>
+
+        <textarea name="" id="" cols="30" rows="10">
+        </textarea>
+        <button type="submit">Send</button>
+      </FormContainer>  
+      </form>
+    </Layout>
 )
 
 export default Contact
